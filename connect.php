f1adbe956fca0c8dca1abe013af48f2bd42ece60
Thomas Friedrich <tomf@mindtouch.com>
2016-11-28 02:20:34 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-  mysql_connect('sql205.byethost10.com', 'b10_18927370', 'Werty99');
-  mysql_select_db('b10_18927370_db');
+  mysql_connect('sql205.byethost10.com', 'b10_1892173720', 'Abcd1234');
+  mysql_select_db('b10_1892173720_db');
 ?>
